77e92b9bad18ad92405c370fdf768d75ef68b762
Freddy Wicaksono <59552422+freddywicaksono@users.noreply.github.com>
2023-06-28 16:13:36 +0700

    Add files via upload

---
+++ WithToken/Client/Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgvData = New System.Windows.Forms.DataGridView()
+        Me.txtProdi = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtJK = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtNIM = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvData
+        '
+        Me.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvData.Location = New System.Drawing.Point(12, 198)
+        Me.dgvData.Name = "dgvData"
+        Me.dgvData.Size = New System.Drawing.Size(357, 183)
+        Me.dgvData.TabIndex = 23
+        '
+        'txtProdi
+        '
+        Me.txtProdi.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtProdi.Location = New System.Drawing.Point(116, 150)
+        Me.txtProdi.Name = "txtProdi"
+        Me.txtProdi.Size = New System.Drawing.Size(100, 27)
+        Me.txtProdi.TabIndex = 22
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 153)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(52, 22)
+        Me.Label4.TabIndex = 21
+        Me.Label4.Text = "Prodi"
+        '
+        'txtJK
+        '
+        Me.txtJK.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtJK.Location = New System.Drawing.Point(116, 105)
+        Me.txtJK.Name = "txtJK"
+        Me.txtJK.Size = New System.Drawing.Size(100, 27)
+        Me.txtJK.TabIndex = 20
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 108)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(31, 22)
+        Me.Label3.TabIndex = 19
+        Me.Label3.Text = "JK"
+        '
+        'txtNama
+        '
+        Me.txtNama.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNama.Location = New System.Drawing.Point(116, 62)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(100, 27)
+        Me.txtNama.TabIndex = 18
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 65)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 22)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "Nama"
+        '
+        'txtNIM
+        '
+        Me.txtNIM.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNIM.Location = New System.Drawing.Point(116, 20)
+        Me.txtNIM.Name = "txtNIM"
+        Me.txtNIM.Size = New System.Drawing.Size(100, 27)
+        Me.txtNIM.TabIndex = 16
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 22)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "NIM"
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(276, 82)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(93, 33)
+        Me.btnDelete.TabIndex = 14
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(276, 43)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(93, 33)
+        Me.btnClear.TabIndex = 13
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(276, 4)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(93, 33)
+        Me.btnAdd.TabIndex = 12
+        Me.btnAdd.Text = "Submit"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(380, 388)
+        Me.Controls.Add(Me.dgvData)
+        Me.Controls.Add(Me.txtProdi)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtJK)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtNama)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtNIM)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents dgvData As DataGridView
+    Friend WithEvents txtProdi As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtJK As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtNIM As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnAdd As Button
+End Class
---
+++ WithToken/Client/MyMod.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Security.Cryptography
+Imports System.Text
+Module MyMod
+    Public api_folder As String = "apptoken"
+    Public mahasiswa_api As String = "http://f0832008.xsph.ru/" & api_folder & "/mahasiswa_api.php"
+    Public mahasiswa_baru As Boolean
+    Public Token As String = GetToken()
+
+    Public Function GetToken() As String
+        Dim valid_token As String
+        Dim kode As String = "UMC.AC.ID"
+        Dim today As DateTime = DateTime.Today
+        Dim formattedDate As String = today.ToString("yyyy-MM-dd")
+        valid_token = GetMD5Hash(kode & formattedDate)
+        Return valid_token
+    End Function
+
+    Public Function GetMD5Hash(ByVal input As String) As String
+        Dim md5Hasher As MD5 = MD5.Create()
+
+        ' Convert the input string to a byte array and compute the hash.
+        Dim data As Byte() = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input))
+
+        ' Create a StringBuilder to collect the bytes and create the hash string.
+        Dim sBuilder As New StringBuilder()
+
+        ' Loop through each byte of the hashed data and format each one as a hexadecimal string.
+        For i As Integer = 0 To data.Length - 1
+            sBuilder.Append(data(i).ToString("x2"))
+        Next
+
+        ' Return the hexadecimal string.
+        Return sBuilder.ToString()
+    End Function
+End Module
